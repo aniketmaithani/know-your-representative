798e84cb15e21e75faed47d4a3c8634a0e38b442
aniketmaithani <maithani.aniket@gmail.com>
2015-12-11 17:48:25 +0530

    corrected typo in provisioner/site

--- kyr/complaints/admin.py
+++ kyr/complaints/admin.py
@@ -15,7 +15,6 @@ class ComplaintsAdmin(admin.ModelAdmin):
     search_fields = ['name_of_the_complainee', 'phone_number_of_the_complainee',
                      'description_of_the_problem', 'name_of_the_mp_for_problem_redressal', 'unique_complain_id', ]
 
-
     def photo_of_the_area(self, obj):
         if obj.photo:
             return '<img src="%s"/>' % obj.photo.thumbnail['100x100'].url
